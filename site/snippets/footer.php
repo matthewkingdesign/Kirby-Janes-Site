7fd6151e02751a5d08d1a13e98d9d8abb2aa9e77
matthewkingdesign <hello@matthewking.design>
2020-05-23 11:58:22 +0100

    sprted postion of menu

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,12 +1,15 @@
 <!-- This is our footer file where everything closes. -->
 <!-- We are using a php foreach loop to dynamically access the menu. Then using kirby objects to get the correct info from the txt files
 We are only using listed (or numbered folders in the file structure)-->
-        <nav class="menu">
-          <?php foreach($site->children()->listed() as $subpage): ?>
-            <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
-          <?php endforeach ?>
-        </nav>
+
       </div>
     </div>
+    <footer>
+      <nav class="menu">
+        <?php foreach($site->children()->listed() as $subpage): ?>
+          <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
+        <?php endforeach ?>
+      </nav>
+    </footer>  
   </body>
 </html>
