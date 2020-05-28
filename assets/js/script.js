69a0055b586fc50d2932a3995dca1ba1f0f76c81
matthewkingdesign <hello@matthewking.design>
2020-05-28 17:23:20 +0100

    sorted out the nav bar to be the same width as the img. it is now based on the image with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
+// This is the script for Janes Site
+//VARIABLES
+
+
 $(document).ready(function(){
     $('.contentWrapper').fadeIn(400);
     $('a').click(function(){
       $('.contentWrapper').fadeOut(200);
     });
-    if ($('#paintingPostImg').hasClass('portrait')) {
-          console.log('hello world');
-      $('#prevNavTag').css('margin-left', '10%');
-      // $('.paintingPostPrevNext').css('width', '30%');
-    }
+    var $imgWidth = $('#paintingPostImg').width();
+    console.log($imgWidth);
+    $('.paintingPostPrevNext').css('width', $imgWidth);
 });
+
+$(window).on('load', function() {
+  console.log('hello window');
+});
+
+// if ($('#paintingPostImg').hasClass('portrait')) {
+//       console.log('hello world');
+//   $('#prevNavTag').css('margin-left', '10%');
+//   // $('.paintingPostPrevNext').css('width', '30%');
+// }
+
+
+// $('.paintingImg').css({
+//   'width':$imgWidth+'px'
+// });
