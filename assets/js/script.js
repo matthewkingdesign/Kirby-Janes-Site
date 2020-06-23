a392b8564a4f9e01f526a459150afb974c9af2ca
matthewkingdesign <hello@matthewking.design>
2020-06-23 10:31:58 +0100

    sorted out script to load on windwo load. The nav bar on paintings post and the about page on phone are now rendering correctly with js var as css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
 // This is the script for Janes Site
-
 $(document).ready(function(){
 
-  // Fades the content when going throught the site
-    $('.contentWrapper').fadeIn(400);
-    $('a').click(function(){
-      $('.contentWrapper').fadeOut(200);
-    });
-  // base width of nav bar on the width of the img
-    var $imgWidth = $('#paintingPostImg').width();
-    console.log($imgWidth);
-    var $imgHeight = $('#paintingPostImg').height();
-    console.log($imgHeight);
-    $('.paintingPostPrevNext').css('width', $imgWidth);
-    var mq = window.matchMedia( "(max-width: 1000px)" );
-    if (mq.matches) {
-      console.log("helllllo there");
-      $('.leftContent').css('margin-top', $imgHeight + 50);
-    }
-});
 
+});
 //check window load works. Not sure yet as to why this is important
 $(window).on('load', function() {
   console.log('hello window');
+  var $imgWidth = $('#paintingPostImg').width();
+  console.log($imgWidth);
+    // base width of nav bar on the width of the img
+  $('.paintingPostPrevNext').css('width', $imgWidth);
+  var $imgHeight = $('#paintingPostImg').height();
+  console.log($imgHeight);
+  var mq = window.matchMedia( "(max-width: 1000px)" );
+  if (mq.matches) {
+    console.log("helllllo there");
+    $('.leftContent').css('margin-top', $imgHeight + 50);
+  }
 });
