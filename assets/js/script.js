6aa6dce3d0e5bcd8b6dc24264d9a60f6813cc68a
matthewkingdesign <hello@matthewking.design>
2020-06-29 18:05:51 +0100

    sorted out animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 // This is the script for Janes Site
 $(document).ready(function(){
-
-
+    $( ".rightContent, .paintinsGrid, .poemsContainer, .contentWrapper, .paintingsPostWrapper, .homePageImg").animate({
+    opacity: 1
+  }, 500);
+    $( ".leftContent" ).animate({
+    opacity: 1
+  }, 500);
+  $('a').click(function(e) {
+  e.preventDefault();
+  var linkUrl = $(this).attr('href');
+  setTimeout(function(url) { window.location = url; }, 600, linkUrl);
+});
+  $('a').click(function() {
+    $('.contentWrapper, .homePageImg').animate({
+      opacity: 0
+    }, 600);
+  });
 });
 //check window load works. Not sure yet as to why this is important
 $(window).on('load', function() {
-  console.log('hello window');
   var $imgWidth = $('#paintingPostImg').width();
   console.log($imgWidth);
     // base width of nav bar on the width of the img
